f7b255939e9da495aef230d287472faabc653b14
jpiergal <john.piergallini@lmco.com>
2019-02-10 21:45:12 -0500

    need to test climber

--- src/main/java/org/usfirst/frc/team708/robot/commands/climber/InitiateClimb.java
+++ src/main/java/org/usfirst/frc/team708/robot/commands/climber/InitiateClimb.java
@@ -14,7 +14,6 @@ public class InitiateClimb extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        requires(Robot.climber);
     }
 
     // Called repeatedly when this Command is scheduled to run
